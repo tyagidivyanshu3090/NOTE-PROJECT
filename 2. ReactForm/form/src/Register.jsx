18552b745aa798535942568054858da9d1b165c6
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-07-01 22:54:32 +0530

    Regex code for useName and Password

--- 2. ReactForm/form/src/Register.jsx
+++ 2. ReactForm/form/src/Register.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+// React Hooks
 import { useState, useRef, useEffect } from "react";
+// FontAwesome
 import {
   faCheck,
   faTimes,
   faInfoCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// Regex
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 const Register = () => {
   return <div>Register</div>;
